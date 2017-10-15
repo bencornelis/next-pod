967efd4a2f93a9fb12c3989e590434eb04fa13a6
Ben Cornelis <bencornelis02@gmail.com>
2017-10-14 19:56:09 -0700

    create base project structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        Hello world!
+      </div>
+    );
+  }
+}
+
+export default App;
